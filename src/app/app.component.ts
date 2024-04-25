90a9907be16bf45178c0117cd44dac51bfdb78d4
alexandrevilhena <ale.carmina@hotmail.com>
2024-04-25 10:23:18 -0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnimalPageComponent } from "./animal-page/animal-page.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AnimalPageComponent]
 })
 export class AppComponent {
   title = 'my-app';
